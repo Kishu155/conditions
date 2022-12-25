908465be3656a73f8642a136e857f678bf93cd0f
krishnakanani1995@gmail.com <krishnakanani1995@gmail.com>
2022-12-25 22:26:29 +0000

    second commit

---
+++ Conditionifelse.java
@@ -0,0 +1,18 @@
+package conditions;
+import java.util.*;
+public class Conditionifelse {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if (age>18) {
+            System.out.println("adult");
+        }
+            else
+            {
+                System.out.println("not adult");
+            }
+
+
+    }
+
+}
